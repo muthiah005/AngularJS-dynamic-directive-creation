17ee5e09fa53b399e645c39bc9d0bf3ca74f2e55
Muthiah M <Muthiah.m@CEINDIA.LOCAL>
2020-01-28 15:22:27 +0530

    - Directive form and validation added - Minor css fixes

--- app/src/directives/listItems.js
+++ app/src/directives/listItems.js
@@ -18,28 +18,40 @@ app.directive('listItems', function($rootScope, $compile) {
 });
 
 app.directive("item", function() {
-	return {// {{text}}
+	return {
 	template:`<div class='box_item'>
-					<form>
+					<form name="loginForm" ng-submit="submitForm(loginForm.$valid)" novalidate>
 					  <div class="form-group">
-					    <label for="exampleInputEmail1">Email address</label>
-					    <input type="email" class="custom_txt form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-					    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+					    <label for="email">Email address</label>
+					    <input type="email" class="custom_txt form-control" 
+					    id="email" aria-describedby="emailHelp" 
+					    placeholder="Enter email"
+					    ng-model="loginForm.email" required>
 					  </div>
+
 					  <div class="form-group">
-					    <label for="exampleInputPassword1">Password</label>
-					    <input type="password" class="custom_txt form-control" id="exampleInputPassword1" placeholder="Password">
+					    <label for="password">Password</label>
+					    <input type="password" 
+					    class="custom_txt form-control"
+					    id="password" placeholder="Password"
+					    ng-model="loginForm.password" required>
 					  </div>
-					  <div class="form-group form-check">
-					    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-					    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-					  </div>
-					  <button type="submit" class="custom_btn btn btn-primary">Submit</button>
+					  <button type="submit" ng-disabled="loginForm.$invalid" class="custom_btn btn btn-primary">Submit</button>
 					</form>
 			  </div>`,
     controllerAs: "testController",
   	controller: function($element,$scope) {
     	$scope.text = "Directive";
+
+    	$scope.loginForm = {};
+
+    	  $scope.submitForm = function(isValid) {
+		    if (isValid) {
+		      console.debug($scope.loginForm.email,$scope.loginForm.password);
+		    }
+		  };
+
+
     	$scope.removeDirective = function(e) {
 		  //remove element and also destroy the scope that element
 		  $element.remove();
